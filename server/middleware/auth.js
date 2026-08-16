3a7772474dd713840b8ecc90b11c11dcc5058706
carlosflorianohlade <carlos.hlade@gmail.com>
2026-08-16 16:06:11 +0200

    rimosso codice basato su env file

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'bike_training_secret_key_2026';
+const JWT_SECRET = 'bike_training_secret_key';
 
 function authenticateToken(req, res, next) {
     const token = req.cookies.token;
