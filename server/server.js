6e19116d4e54fd21f0d9737fa70175d84ab91fbf
carlosflorianohlade <carlos.hlade@gmail.com>
2026-09-19 22:40:18 +0200

    rimosso codice inutile in server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const statsRoutes = require('./routes/statsRoutes');
 const goalsRoutes = require('./routes/goalsRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
